27b75c5f2eec662b850c167adafdcc66811de7c2
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-03-26 14:41:00 +0200

    single

--- wp-content/themes/example-theme/inc/article-function.php
+++ wp-content/themes/example-theme/inc/article-function.php
@@ -6,7 +6,7 @@ function generate_article( $products ): void {
 			?>
             <article class="product">
 				<?php
-				the_post_thumbnail();
+				the_post_thumbnail('thumbnail');
 				the_title( '<h3>', '</h3>' );
 				$excerpt = get_the_excerpt();
 				?>
@@ -15,6 +15,7 @@ function generate_article( $products ): void {
 					echo substr( $excerpt, 0, 100 );
 					?>...
                 </p>
+                <a href="<?php the_permalink(); ?>">Read more</a>
             </article>
 		<?php
 		endwhile;
